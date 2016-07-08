3337285ef1bd7c364e6a22820a886dcce66b31e2
Carlos Lopez Jr <clshortfuse@gmail.com>
2016-07-08 22:15:43 +0000

    feat(theming): add contrast opacity values for all color types and hues
    Add opacity keyword support (`secondary`, `icon`, `disabled`, `hint`, `divider`)
    Deprecate documentation of `foreground-*` in favor of `background-default-contrast-*`
    Allow `foregroundPallete` to override colors on default background and hues of equal contrast type

--- src/core/services/theming/theming.js
+++ src/core/services/theming/theming.js
@@ -134,6 +134,8 @@ function detectDisabledThemes($mdThemingProvider) {
  * {{primary-contrast}} - Generates .md-hue-1, .md-hue-2, .md-hue-3 with configured contrast (ie. text) color shades set for each hue
  * {{primary-contrast-0.7}} - Apply 0.7 opacity to each of the above rules
  * {{primary-contrast-divider}} - Apply divider opacity to contrast color
+ * {{background-default-contrast}} - Apply primary text color for contrasting with default background
+ * {{background-50-contrast-icon}} - Apply contrast color for icon on background's shade 50 hue
  *
  * Foreground expansion: Applies rgba to black/white foreground text
  *
